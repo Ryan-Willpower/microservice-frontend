e12341bad99eb1ab3a738384a822c84a67f09a09
Ryan-Willpower <surasak.c.work@gmail.com>
2019-10-21 23:55:02 +0700

    [navbar]  add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import { Index } from "./pages";
-import { Example } from "./commons";
 import { GlobalStyle } from "./commons/globalStyle";
+import { Navbar } from "./commons/navbar";
+import { Container } from "./commons/container";
 
 export const App: React.FC = () => {
   return (
     <Router>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-      </ul>
+      <Container>
+        <Navbar />
 
-      <Example />
-
-      <Switch>
-        <Route path="/">
-          <Index />
-        </Route>
-      </Switch>
+        <Switch>
+          <Route path="/">
+            <Index />
+          </Route>
+        </Switch>
+      </Container>
 
       <GlobalStyle />
     </Router>
