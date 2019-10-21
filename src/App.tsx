527a63b2d0fb257ea3e629dfd8c6dbde9b92b0aa
Ryan-Willpower <surasak.c.work@gmail.com>
2019-10-21 22:30:31 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import { Index } from "./pages";
+import { Example } from "./commons";
+import { GlobalStyle } from "./commons/globalStyle";
+
+export const App: React.FC = () => {
+  return (
+    <Router>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+      </ul>
+
+      <Example />
+
+      <Switch>
+        <Route path="/">
+          <Index />
+        </Route>
+      </Switch>
+
+      <GlobalStyle />
+    </Router>
+  );
+};
---
+++ src/commons/globalStyle.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Global, css } from "@emotion/core";
+
+const globalCSS = css`
+  html,
+  body {
+    margin: 0;
+    padding: 0;
+    font-size: 16px;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+`;
+
+export const GlobalStyle = () => <Global styles={globalCSS} />;
