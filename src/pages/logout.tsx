f7314a24f4fc269fe9e70e04e6fe758cf8ea443f
Ryan-Willpower <surasak.c.work@gmail.com>
2019-10-25 03:56:56 +0700

    [cookie] fix bug that cookie not remove

--- src/pages/logout.tsx
+++ src/pages/logout.tsx
@@ -4,7 +4,10 @@ import { Redirect } from 'react-router-dom'
 import { cookies } from '../utils/cookies'
 
 export const Logout: React.FC = () => {
-  cookies.remove('userdata')
+  // remove cookies until it gone
+  while (cookies.get('userdata')) {
+    cookies.remove('userdata')
+  }
   window.location.href = '/'
   return <Redirect from='/logout' to='/' />
 }
